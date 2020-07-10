ec0283fc2db5ed4b0a1a3a6f59dc14bd064cab18
Michael Baranov <developer.bami@gmail.com>
2020-07-10 20:01:48 +0800

    Update runme.cmd

--- runme.cmd
+++ runme.cmd
@@ -1,3 +1,15 @@
 
- java -jar build\libs\bugs-classification-v1.jar prepare.es  ./test/dataset   ./test/es  code  --ngramsize=6
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\CE12800\2020\src_simple   D:\Work\CE12800\2020\result\ngram D:\Work\CE12800\2020\list.txt  ngram  --ngramsize=5
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar cluster.es  D:\Work\CE12800\2020\result\ngram  D:\Work\CE12800\2020\result D:\Work\CE12800\2020\list.txt ngram --algorithm=jac 
+
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\Router\2020\src_simple   D:\Work\Router\2020\result\ngram D:\Work\Router\2020\list.txt  ngram  --ngramsize=5
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\Router\2020\src_simple   D:\Work\Router\2020\result\ngram D:\Work\Router\2020\list.txt  ngram  --ngramsize=5
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\Router\2020\src_simple   D:\Work\Router\2020\result\ngram D:\Work\Router\2020\list.txt  ngram  --ngramsize=5
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\Router\2020\src_simple   D:\Work\Router\2020\result\ngram D:\Work\Router\2020\list.txt  ngram  --ngramsize=5
+rem d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar cluster.es  D:\Work\Router\2020\result\ngram  D:\Work\Router\2020\result D:\Work\Router\2020\list.txt ngram --algorithm=jac 
+
+
+d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar prepare.es  D:\Work\WDM\2020\src_simple   D:\Work\WDM\2020\result\ngram D:\Work\WDM\2020\list.txt  ngram  --ngramsize=5
+d:\j\bin\java -Xmx12G -Dfile.encoding=UTF-8 -jar build\libs\bugs-classification-v1.jar cluster.es  D:\Work\WDM\2020\result\ngram  D:\Work\WDM\2020\result D:\Work\WDM\2020\list.txt ngram --algorithm=jac 
+
 pause
